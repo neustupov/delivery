f9f5dcd7d23a68dce976080e764806ead8a1c58a
Dmitry_Neustupov <neustupov@yandex.ru>
2020-01-11 21:31:46 +0300

    add test for ticket

--- model/src/main/java/org/neustupov/delivery/app/model/entity/kitchen/Ticket.java
+++ model/src/main/java/org/neustupov/delivery/app/model/entity/kitchen/Ticket.java
@@ -1,14 +1,17 @@
 package org.neustupov.delivery.app.model.entity.kitchen;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.MappedSuperclass;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.neustupov.delivery.app.model.entity.base.AbstractEntity;
 
 @Data
+@NoArgsConstructor
 @Entity
 @MappedSuperclass
 @EqualsAndHashCode(callSuper = true)
@@ -18,4 +21,11 @@ public class Ticket extends AbstractEntity {
   private LocalDateTime requestedDeliveryTime;
   private LocalDateTime preparedByTime;
   private Set<TicketLineItem> ticketLineItems;
+
+  public void addTicket(TicketLineItem ticketLineItem){
+    if(ticketLineItems == null){
+      ticketLineItems = new HashSet<>();
+    }
+    ticketLineItems.add(ticketLineItem);
+  }
 }
---
+++ model/src/test/java/org/neustupov/delivery/app/model/entity/kitchen/TicketTest.java
@@ -0,0 +1,20 @@
+package org.neustupov.delivery.app.model.entity.kitchen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TicketTest {
+
+  @Test
+  void addTicket() {
+    Ticket ticket = new Ticket();
+    TicketLineItem ticketLineItem = new TicketLineItem(1, "apple");
+    ticket.addTicket(ticketLineItem);
+    assertTrue(containsTicketLineItem(ticket, ticketLineItem));
+  }
+
+  private boolean containsTicketLineItem(Ticket ticket, TicketLineItem ticketLineItem) {
+    return ticket.getTicketLineItems().contains(ticketLineItem);
+  }
+}
