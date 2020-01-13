ef3e4cf691fb91eb45213caeba10857a7771c9f6
Dmitry_Neustupov <neustupov@yandex.ru>
2020-01-13 22:25:35 +0300

    added import repo and service config, added spring context dependency in repo and service module

--- repository/src/main/java/org/neustupov/delivery/app/RepositoryContextConfig.java
+++ repository/src/main/java/org/neustupov/delivery/app/RepositoryContextConfig.java
@@ -2,11 +2,9 @@ package org.neustupov.delivery.app;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
-@Import({ServiceContextConfig.class, RepositoryContextConfig.class})
 @Configuration
 @ComponentScan
-public class AppConfig {
+public class RepositoryContextConfig {
 
 }
--- repository/src/main/java/org/neustupov/delivery/app/repository/restaurant/RestaurantRepository.java
+++ repository/src/main/java/org/neustupov/delivery/app/repository/restaurant/RestaurantRepository.java
@@ -1,9 +1,12 @@
 package org.neustupov.delivery.app.repository.restaurant;
 
+import java.util.List;
 import org.neustupov.delivery.app.model.entity.restaurant.Restaurant;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
+
+  List<Restaurant> findByName();
 }
