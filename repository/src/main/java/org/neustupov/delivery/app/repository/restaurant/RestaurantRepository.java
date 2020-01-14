aee131bf535108816b6a5b08f748f1f98a9b25eb
Dmitry_Neustupov <neustupov@yandex.ru>
2020-01-14 07:59:29 +0300

    added impl class for rest repo

--- repository/src/main/java/org/neustupov/delivery/app/repository/restaurant/RestaurantRepository.java
+++ repository/src/main/java/org/neustupov/delivery/app/repository/restaurant/RestaurantRepository.java
@@ -3,9 +3,7 @@ package org.neustupov.delivery.app.repository.restaurant;
 import java.util.List;
 import org.neustupov.delivery.app.model.entity.restaurant.Restaurant;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
 
   List<Restaurant> findByName();
